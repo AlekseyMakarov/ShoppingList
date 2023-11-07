f2f8c9ba59f8018a2c920a3b9c412820325792ad
Aleksey <leha2546461@rambler.ru>
2023-11-07 19:50:04 +0300

    Refactor

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,29 +1,23 @@
 package com.example.shoppinglist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
     private lateinit var viewModel: MainViewModel
-
     private lateinit var shopListAdapter: RecyclerViewShopListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setupRecyclerView()
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
-
         }
     }
 
@@ -52,7 +46,6 @@ class MainActivity : AppCompatActivity() {
                 val shopItem = shopListAdapter.currentList[viewHolder.adapterPosition]
                 viewModel.deleteShopItem(shopItem)
             }
-
         }
         val itemTouchHelper = ItemTouchHelper(callback)
         itemTouchHelper.attachToRecyclerView(rvShopList)
--- app/src/main/java/com/example/shoppinglist/presentation/RecyclerViewShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/RecyclerViewShopListAdapter.kt
@@ -1,33 +1,23 @@
 package com.example.shoppinglist.presentation
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.AsyncListDiffer
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
 class RecyclerViewShopListAdapter :
-    ListAdapter<ShopItem, RecyclerViewShopListAdapter.ShopListViewHolder>(ShopItemDiffCallback()) {
+    ListAdapter<ShopItem, ShopListViewHolder>(ShopItemDiffCallback()) {
     companion object {
         const val ENABLED_TYPE = 0
         const val DISABLED_TYPE = 1
         const val MAX_POOL_SIZE = 15
     }
 
-    var count = 0
-
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopListViewHolder {
-        Log.d("OnCreateViewHolder", "count: ${++count}")
         val layoutType = when (viewType) {
             ENABLED_TYPE -> R.layout.item_shop_enabled
             DISABLED_TYPE -> R.layout.item_shop_disabled
@@ -52,10 +42,5 @@ class RecyclerViewShopListAdapter :
 
     override fun getItemViewType(position: Int): Int {
         return if (getItem(position).enabled) ENABLED_TYPE else DISABLED_TYPE
-    }
-
-    class ShopListViewHolder(val view: View) : ViewHolder(view) {
-        val tvName: TextView = view.findViewById<TextView>(R.id.tv_name)
-        val tvCount: TextView = view.findViewById<TextView>(R.id.tv_count)
     }
 }
