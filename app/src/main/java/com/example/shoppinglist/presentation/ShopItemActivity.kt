39781934c6b8312f636c69148d0c3183eaa5d472
Aleksey <leha2546461@rambler.ru>
2023-11-23 18:06:07 +0300

    ShopItemActivity edited

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -1,28 +1,160 @@
 package com.example.shoppinglist.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty0
 
 class ShopItemActivity : AppCompatActivity() {
     private lateinit var viewModel: ShopItemViewModel
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: TextInputEditText
+    private lateinit var etCount: TextInputEditText
+    private lateinit var saveButton: Button
+    private var shopItemId = ShopItem.UNDEFINED_ID
+    private lateinit var mode: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
         viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews()
+        parseIntent()
+        setListeners()
+        setObservers()
+
     }
 
-    companion object{
+
+    companion object {
+
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_KEY = "mode"
+        private const val SHOP_ITEM_KEY = "shop_item_id"
 
 
-        fun newIntentAddItem()
-            // TODO:
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(MODE_KEY, MODE_ADD)
+            return intent
         }
 
-        fun newIntentEditItem()
-            // TODO:
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(MODE_KEY, MODE_EDIT)
+            intent.putExtra(SHOP_ITEM_KEY, shopItemId)
+            return intent
+        }
+    }
+
+    private fun initViews() {
+        tilName = findViewById(R.id.til_name)
+        tilCount = findViewById(R.id.til_count)
+        etName = findViewById(R.id.et_name)
+        etCount = findViewById(R.id.et_count)
+        saveButton = findViewById(R.id.save_button)
+    }
+
+    private fun parseIntent() {
+        if (!intent.hasExtra(MODE_KEY))
+            throw Exception("Mode in intent is absent")
+        when (intent.getStringExtra(MODE_KEY)) {
+            MODE_EDIT -> {
+                mode = MODE_EDIT
+                if (!intent.hasExtra(SHOP_ITEM_KEY))
+                    throw Exception("Shop item id in intent is absent")
+                shopItemId = intent.getIntExtra(SHOP_ITEM_KEY, ShopItem.UNDEFINED_ID)
+                viewModel.getShopItem(shopItemId)
+            }
+            MODE_ADD -> {
+                mode = MODE_ADD
+            }
+            else -> {
+                throw Exception("Undefined mode")
+            }
+        }
+
+    }
+
+    private fun setListeners() {
+        if (this::mode.isInitialized)
+            when (mode) {
+                MODE_ADD -> saveButton.setOnClickListener {
+                    viewModel.addShopItem(
+                        etName.text.toString(),
+                        etCount.text.toString()
+                    )
+                }
+                MODE_EDIT -> saveButton.setOnClickListener {
+                    viewModel.editShopItem(
+                        etName.text.toString(),
+                        etCount.text.toString()
+                    )
+                }
+            }
+        etName.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetInputNameError()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        etCount.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetInputCountError()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun setObservers() {
+        viewModel.shopItem.observe(this) {
+            etCount.setText(it.count.toString())
+            etName.setText(it.name)
+        }
+        viewModel.inputNameError.observe(this) {
+            if (it)
+                tilName.error = getString(R.string.error_input_name)
+            else
+                tilName.error = null
+
+        }
+        viewModel.inputCountError.observe(this) {
+            if (it)
+                tilCount.error = getString(R.string.error_input_count)
+            else
+                tilCount.error = null
+        }
+        viewModel.shouldCloseActivity.observe(this) {
+            finish()
         }
     }
 }
